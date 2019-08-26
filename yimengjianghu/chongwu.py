629e26723a27e195a6f6d58971d62caade0bfd41
许树真 <xushuzhen1992@163.com>
2019-08-26 21:18:58 +0800

    斗宠物

---
+++ yimengjianghu/chongwu.py
@@ -0,0 +1,30 @@
+import sys
+
+sys.path.append("../")
+from common.android_handle import *
+from yimengjianghu.config.chongwu import *
+
+
+def start_f(is_lose=False):
+    print("点击宠物pk")
+    handle.list_click(start)
+    print("选择宠物")
+    if is_lose:
+        handle.list_click(select1)
+        print("认输")
+        handle.list_click(out)
+    else:
+        handle.list_click(select)
+    print("点击结束")
+    handle.list_click(end)
+
+
+handle = AndroidHandle()
+
+if __name__ == "__main__":
+    i = sys.argv[1]
+    while True:
+        if i == "2":
+            start_f(False)
+        else:
+            start_f()
